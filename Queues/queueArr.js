6898703cd1b988582892a0e8d18b745d9f5a79e3
dye784 <damon.ye91@gmail.com>
2016-12-19 13:44:01 -0500

    finished queLL

---
+++ Queues/queueArr.js
@@ -0,0 +1,27 @@
+class QueueArr {
+  constructor() {
+
+  }
+
+  isEmpty() {
+
+  }
+
+  // add to tail
+  enqueue(str) {
+
+  }
+
+  // remove from head
+  dequeue() {
+  }
+}
+
+
+const stack = new QueueArr()
+stack.enqueue("hello")
+stack.enqueue("goodbye")
+
+console.log(stack)
+
+module.exports = QueueArr
---
+++ Queues/queueLL.js
@@ -0,0 +1,46 @@
+class QueueLL {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  // check if stack has anything
+  isEmpty() {
+    return this.head === null;
+  }
+
+  // add to tail
+  enqueue(str) {
+    const newNode = new Node(str);
+    const oldTail = this.tail;
+    this.tail = newNode
+
+    if(this.isEmpty()) this.head = newNode;
+    else oldTail.next = this.tail
+  }
+
+  // remove from head
+  dequeue() {
+    const str = this.head.str
+    this.head = this.head.next
+
+    if(isEmpty()) this.tail = null;
+
+    return str;
+  }
+}
+
+class Node {
+  constructor(str) {
+    this.str = str;
+    this.next = null;
+  }
+}
+
+const stack = new QueueLL()
+stack.enqueue("hello")
+stack.enqueue("goodbye")
+
+console.log(stack)
+
+module.exports = QueueLL
