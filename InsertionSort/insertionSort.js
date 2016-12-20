356c29a7fe3f3addbb560643a585ef7e6b7112b3
dye784 <damon.ye91@gmail.com>
2016-12-19 23:32:59 -0500

    added insertionSort

---
+++ InsertionSort/insertionSort.js
@@ -0,0 +1,30 @@
+class InsertionSort {
+  constructor(arr) {
+    this.array = arr
+  }
+
+  sort() {
+    let length = this.array.length
+
+    for (var i = 0; i < length; i++) {
+      for (var j = i; j < length; j--) {
+        if(this.isLess(j, j - 1)) this.exchange(j, j - 1)
+        else break;
+      }
+    }
+
+    return this.array
+  }
+
+  isLess(idx1, idx2) {
+    return this.array[idx1] < this.array[idx2]
+  }
+
+  exchange(idx1, idx2) {
+    let temp = this.array[idx1]
+    this.array[idx1] = this.array[idx2]
+    this.array[idx2] = temp;
+  }
+}
+
+module.exports = InsertionSort;
