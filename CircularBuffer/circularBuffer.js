1b793c0cd12e6af6746614652a1a5bcbce5feb78
dye784 <damon.ye91@gmail.com>
2016-12-26 16:34:30 -0500

    completed deq

--- CircularBuffer/circularBuffer.js
+++ CircularBuffer/circularBuffer.js
@@ -5,7 +5,9 @@ class CircularBuffer {
     this.head = this.tail = this.size = 0;
   }
 
-  push(data) {
+  enqueue(data) {
+    this.tail = (this.head + this.size) % this.capacity // adjust the tail
+
     let nextIdx = this.head + 1; // next index we are adding into the array
 
     if (nextIdx >= this.capacity) nextIdx = 0; // loop around to beginning of array
@@ -13,12 +15,22 @@ class CircularBuffer {
 
     this.buffer[this.head] = data; // put data in the array
     this.head = nextIdx; // set the pointer to the new index;
+
   }
 
-  pop() {
-    let data = this.buffer[this.tail]; // the item to be removed
+  dequeue() {
+    if (this.size === 0) return undefined;
 
-    return data;
+    let data = this.buffer[this.tail]; // the item to be removed
+    this.buffer[this.tail] = null; // delete item
+
+    let nextIdx = this.tail + 1; // increment the tail
+
+    if (nextIdx >= this.capacity) nextIdx = 0; // check if tail circles around
+    if (this.size > 0) this.size--; // decrement size
+    this.tail = nextIdx; // set new tail
+
+    return data; // return the data
   }
 
   // shift(element) {
--- CircularBuffer/circularBuffer.test.js
+++ CircularBuffer/circularBuffer.test.js
@@ -32,24 +32,28 @@ describe.only('CircularBuffer', () => {
     })
   })
 
-  describe('push', () => {
+  describe('enqueue', () => {
+    it('should be a function on the class', () => {
+      expect(newCB).to.have.property('enqueue')
+      expect(newCB.enqueue).to.be.instanceof(Function)
+    })
 
     it('should increment the size when called but not if it is greater than or equal to the capacity', () => {
-      newCB.push(1)
+      newCB.enqueue(1)
       expect(newCB.size).to.deep.equal(1)
 
       newCB.size = 8;
-      newCB.push(2)
+      newCB.enqueue(2)
       expect(newCB.size).to.deep.equal(8)
     })
 
     it('should increment the head when called', () => {
-      newCB.push(1)
+      newCB.enqueue(1)
       expect(newCB.head).to.deep.equal(1)
     })
 
     it('should insert one item into the buffer at the next index', () => {
-      newCB.push(1)
+      newCB.enqueue(1)
       let solution = Array(8)
       solution[0] = 1;
       expect(newCB.buffer).to.deep.equal(solution)
@@ -59,7 +63,7 @@ describe.only('CircularBuffer', () => {
       let arrBefore = [0,1,2,3,4,5,6,7];
       newCB.buffer = arrBefore;
       newCB.head = 0;
-      newCB.push(8);
+      newCB.enqueue(8);
       let solution = [8,1,2,3,4,5,6,7]
       expect(newCB.buffer).to.deep.equal(solution);
     })
@@ -68,42 +72,74 @@ describe.only('CircularBuffer', () => {
       let arrBefore = [8,1,2,3,4,5,6,7];
       newCB.buffer = arrBefore;
       newCB.head = 1;
-      newCB.push(9);
+      newCB.enqueue(9);
       let solution = [8,9,2,3,4,5,6,7]
       expect(newCB.buffer).to.deep.equal(solution);
     })
 
-    it('should work with MANY pushes', () => {
-       for(let i = 0; i < 42; i++) {
-        newCB.push(i);
+    it('should work with MANY enqueuees', () => {
+      for(let i = 0; i < 42; i++) {
+        newCB.enqueue(i);
       }
       let solution = [40,41,34,35,36,37,38,39]
       expect(newCB.buffer).to.deep.equal(solution);
     })
 
-    it('should work with MANY pushes', () => {
+    it('should work with MANY enqueuees', () => {
       for(let i = 0; i < 20; i++) {
-        newCB.push(i);
+        newCB.enqueue(i);
       }
+      console.log(newCB.tail)
       let solution = [16,17,18,19,12,13,14,15]
       expect(newCB.buffer).to.deep.equal(solution);
     })
   })
 
-  // describe('pop', () => {
-  //   it('should return the last item added', () => {
-  //     newCB.push(1);
-  //     expect(newCB.pop()).to.equal(1);
-  //   })
+  describe('dequeue', () => {
+    it('should be a function on the class', () => {
+      expect(newCB).to.have.property('dequeue')
+      expect(newCB.dequeue).to.be.instanceof(Function)
+    })
 
-  //   it('should', () => {})
-  // })
+    it('should return undefined when trying to dequeue with a size of 0', () => {
+      expect(newCB.dequeue()).to.be.undefined;
+    })
 
-  // describe('get', () => {
+    it('should decrement the size', () => {
+      newCB.size = 1;
+      newCB.buffer = [1]
+      newCB.dequeue()
+      expect(newCB.size).to.equal(0);
+    })
 
-  // })
+    it('should return the first item added that was not replaced and set the element to null', () => {
+      newCB.buffer = [0,1,2,3,4,5,6,7]
+      newCB.tail = 0;
+      newCB.size = 8;
+      expect(newCB.dequeue()).to.equal(0);
+      expect(newCB.dequeue()).to.equal(1);
+      expect(newCB.dequeue()).to.equal(2);
+      expect(newCB.dequeue()).to.equal(3);
+      expect(newCB.dequeue()).to.equal(4);
+      expect(newCB.dequeue()).to.equal(5);
+      expect(newCB.dequeue()).to.equal(6);
+      expect(newCB.dequeue()).to.equal(7);
+      expect(newCB.buffer).to.deep.equal([null,null,null,null,null,null,null,null])
+    })
 
-  // describe('unshift', () => {
-
-  // })
+    it('should return the first item added that was not replaced and circle around the array', () => {
+      newCB.buffer = [8,9,2,3,4,5,6,7]
+      newCB.tail = 2;
+      newCB.size = 8;
+      expect(newCB.dequeue()).to.equal(2);
+      expect(newCB.dequeue()).to.equal(3);
+      expect(newCB.dequeue()).to.equal(4);
+      expect(newCB.dequeue()).to.equal(5);
+      expect(newCB.dequeue()).to.equal(6);
+      expect(newCB.dequeue()).to.equal(7);
+      expect(newCB.dequeue()).to.equal(8);
+      expect(newCB.dequeue()).to.equal(9);
+      expect(newCB.buffer).to.deep.equal([null,null,null,null,null,null,null,null])
+    })
+  })
 })
