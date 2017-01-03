23ec3e740c7e83e1fa15b5878fc8291483ae5917
dye784 <damon.ye91@gmail.com>
2017-01-03 14:11:03 -0500

    added comments to graph

--- AdjacencyListGraph/adjacencyListGraph.js
+++ AdjacencyListGraph/adjacencyListGraph.js
@@ -7,29 +7,35 @@ class Graph {
     this.verticesInit();
   }
 
+  // initialize each vertex in verticies array with another array to house connections
   verticesInit() {
     for (var i = 0; i < this.numVertices; i++) {
       this.vertices[i] = []
     }
   }
 
+  // add edges to each corresponding vertex
   addEdge(vertex1, vertex2) {
     this.vertices[vertex1].push(vertex2);
     this.vertices[vertex2].push(vertex1);
   }
 
+  // returns the connections to specific vertex
   adjacentVertices(v) {
     return this.vertices[v]
   }
 
+  // the number of connections to a specific vertex
   degree(v) {
     return this.vertices[v].length;
   }
 
+  // the furthest connection
   maxDegree() {
     return Math.max(...this.vertices.map((arr, index) => this.degree(index)));
   }
 
+  // average connection distance
   averageDegree() {
     const degrees = this.vertices.map((arr, index) => this.degree(index));
     return degrees.reduce((a, b) => a + b) / degrees.length;
