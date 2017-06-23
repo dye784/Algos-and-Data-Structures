66a198a0e35a7a3458b6ee2b631299fa6c22b322
dye784 <damon.ye91@gmail.com>
2017-06-23 15:44:29 -0400

    chore: fix styling

--- BinarySearchTree/BST.js
+++ BinarySearchTree/BST.js
@@ -6,15 +6,15 @@ class BinarySearchTree {
     this.magnitude = 1;
   }
 
-  insert(val){
-    let direction = val < this.value ? 'left' : 'right';
+  insert(val) {
+    const direction = val < this.value ? 'left' : 'right';
     if (this[direction]) this[direction].insert(val);
     else this[direction] = new BinarySearchTree(val);
     this.magnitude++;
   }
 
   // recursive binary search
-  containsRecursive(val){
+  containsRecursive(val) {
     if (this.value === val) return true;
     let direction = val < this.value ? 'left' : 'right';
     if (this[direction]) return this[direction].containsRecursive(val);
@@ -26,16 +26,16 @@ class BinarySearchTree {
     let tree = this;
 
     while (tree !== null) {
-      if (val < tree.value) tree = tree.left
-      else if (val > tree.value) tree = tree.right
-      else return true
+      if (val < tree.value) tree = tree.left;
+      else if (val > tree.value) tree = tree.right;
+      else return true;
     }
 
     return false;
   }
 
   // all in one implementation
-  depthFirstForEach(fn, opt = 'in-order'){
+  depthFirstForEach(fn, opt = 'in-order') {
     if (opt === 'pre-order') fn(this.value);
     if (this.left) this.left.depthFirstForEach(fn, opt);
     if (opt === 'in-order') fn(this.value);
@@ -47,27 +47,27 @@ class BinarySearchTree {
 
   dfsInOrder(fn) {
     if (this.value === null) return;
-    if (this.left) this.left.dfsInOrder(fn)
-    fn(this.value)
-    if (this.right) this.right.dfsInOrder(fn)
+    if (this.left) this.left.dfsInOrder(fn);
+    fn(this.value);
+    if (this.right) this.right.dfsInOrder(fn);
   }
 
   dfsPreOrder(fn) {
     if (this.value === null) return;
-    fn(this.value)
-    if (this.left) this.left.dfsPreOrder(fn)
-    if (this.right) this.right.dfsPreOrder(fn)
+    fn(this.value);
+    if (this.left) this.left.dfsPreOrder(fn);
+    if (this.right) this.right.dfsPreOrder(fn);
   }
 
   dfsPostOrder(fn) {
     if (this.value === null) return;
-    if (this.left) this.left.dfsPostOrder(fn)
-    if (this.right) this.right.dfsPostOrder(fn)
-    fn(this.value)
+    if (this.left) this.left.dfsPostOrder(fn);
+    if (this.right) this.right.dfsPostOrder(fn);
+    fn(this.value);
   }
 
-  breadthFirstForEach(fn){
-    let queue = [this];
+  breadthFirstForEach(fn) {
+    const queue = [this];
     while (queue.length) {
       let current = queue.shift();
       if (current.left) queue.push(current.left);
