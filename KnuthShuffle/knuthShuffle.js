51b615d2f823d6b37f5ad2c362426728d69a6c63
dye784 <damon.ye91@gmail.com>
2017-06-23 15:37:58 -0400

    refactor: es6

--- KnuthShuffle/knuthShuffle.js
+++ KnuthShuffle/knuthShuffle.js
@@ -1,18 +1,17 @@
-
-const knuthShuffle = (deck) => {
-  for(let i = 0; i < deck.length; i++) {
-
-    // i needs to be inclusive
-    let r = Math.floor(Math.random() * (i + 1))
-
-    swap(deck, i, r)
-  }
-}
-
 const swap = (array, idx1, idx2) => {
-  let temp = array[idx1];
+  const temp = array[idx1];
   array[idx1] = array[idx2];
   array[idx2] = temp;
-}
+};
 
-module.exports = knuthShuffle
+const knuthShuffle = (deck) => {
+  for (let i = 0; i < deck.length; i++) {
+
+    // i needs to be inclusive
+    const r = Math.floor(Math.random() * (i + 1));
+    swap(deck, i, r);
+  }
+};
+
+
+module.exports = knuthShuffle;
