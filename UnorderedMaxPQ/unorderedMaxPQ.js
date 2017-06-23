147d898c17560c4e31b292ac8b078a647f57f291
dye784 <damon.ye91@gmail.com>
2017-06-23 15:45:28 -0400

    chore: add semis

--- UnorderedMaxPQ/unorderedMaxPQ.js
+++ UnorderedMaxPQ/unorderedMaxPQ.js
@@ -1,6 +1,4 @@
-
 class UnorderedMaxPQ {
-
   // have a prop array and size equal to input length
   constructor(array) {
     this.array = array;
@@ -9,7 +7,7 @@ class UnorderedMaxPQ {
 
   // checks if empty
   isEmpty() {
-    return this.array.length === 0
+    return this.array.length === 0;
   }
 
   // remove max from array
@@ -18,31 +16,30 @@ class UnorderedMaxPQ {
 
     // iterate through the array
     for (let i = 0; i < this.size; i++) {
-
       // if array[max] is less than array[i] the new max is at index i
-      if(this.isLess(max, i)) max = i
+      if (this.isLess(max, i)) max = i;
     }
 
     // move the max to the end of the array
-    this.exchange(max, this.size - 1)
+    this.exchange(max, this.size - 1);
 
     // return the last element and decrement the size of array
-    return this.array[--this.size]
+    return this.array[--this.size];
   }
 
   // input element into end of array and incement size
   insert(element) {
-    this.array[this.size++] = element
+    this.array[this.size++] = element;
   }
 
   // for more modular approach but not necessary
   isLess(idx1, idx2) {
-    return this.array[idx1] < this.array[idx2]
+    return this.array[idx1] < this.array[idx2];
   }
 
   // same as sorting method
   exchange(idx1, idx2) {
-    let temp = this.array[idx1];
+    const temp = this.array[idx1];
     this.array[idx1] = this.array[idx2];
     this.array[idx2] = temp;
   }
